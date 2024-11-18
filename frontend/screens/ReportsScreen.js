6b92b40cf4b893333f854b68c6352353a6f0ce80
AndreiSusha <Andrey.Susha@gmail.com>
2024-11-18 15:19:16 +0200

    home, settings and report screens has been added

---
+++ frontend/screens/ReportsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ReportsScreen = () => {
+  return (
+    <View>
+      <Text>ReportsScreen</Text>
+    </View>
+  );
+};
+
+export default ReportsScreen;
+
+const styles = StyleSheet.create({});
