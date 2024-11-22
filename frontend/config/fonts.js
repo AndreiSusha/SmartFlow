f3c370272731407f4e29c7f7c6f38d27a64024cd
AndreiSusha <Andrey.Susha@gmail.com>
2024-11-22 12:56:14 +0200

    a config folder and a font file were created to optimize the code

---
+++ frontend/config/fonts.js
@@ -0,0 +1,8 @@
+const customFonts = {
+  'Urbanist-Bold': require('../assets/fonts/Urbanist-Bold.ttf'),
+  'Urbanist-Italic': require('../assets/fonts/Urbanist-Italic.ttf'),
+  'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf'),
+  'Urbanist-SemiBold': require('../assets/fonts/Urbanist-SemiBold.ttf'),
+};
+
+export default customFonts;
