f0496e0d1bcaadff06d92e2182eb6359bd629dd2
AndreiSusha <Andrey.Susha@gmail.com>
2024-11-22 13:09:33 +0200

    file updated

--- frontend/config/fonts.js
+++ frontend/config/fonts.js
@@ -3,6 +3,11 @@ const customFonts = {
   'Urbanist-Italic': require('../assets/fonts/Urbanist-Italic.ttf'),
   'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf'),
   'Urbanist-SemiBold': require('../assets/fonts/Urbanist-SemiBold.ttf'),
+  'Inter-Medium': require('../assets/fonts/Inter-Medium.ttf'),
+  'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
+  'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+  'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+  'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
 };
 
 export default customFonts;
