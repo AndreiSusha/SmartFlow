d80f22eddfbeceb95ef09ea97d38d040c75e0b89
AndreiSusha <Andrey.Susha@gmail.com>
2024-11-26 12:00:05 +0200

    updated

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -6,36 +6,19 @@ import {
   TouchableOpacity,
   ScrollView,
 } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 import ReportCard from '../components/ReportCard';
 import Chart from '../components/Chart';
 
 const HomeScreen = ({ route }) => {
   const { role } = route.params || { role: 'user' };
-  const [selectedOption, setSelectedOption] = useState('Option 1');
 
   return (
     <ScrollView style={styles.container}>
-      {/* Drop down menu */}
-      {/* {role === 'admin' && (
-        <View style={styles.dropdownBox}>
-          <Picker
-            selectedValue={selectedOption}
-            onValueChange={(itemValue) => setSelectedOption(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Option 1" value="Option 1" />
-            <Picker.Item label="Option 2" value="Option 2" />
-            <Picker.Item label="Option 3" value="Option 3" />
-          </Picker>
-        </View>
-      )} */}
-
       {/* Title */}
       <Text style={styles.title}>Consumption Overview</Text>
 
       {/* Chart */}
-      <Chart />
+      <Chart role={role} />
 
       {/* Reports */}
       <View style={styles.subtitleRow}>
@@ -63,17 +46,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E3E3E3',
     padding: 16,
-  },
-  dropdownBox: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    marginBottom: 16,
-    elevation: 4,
-  },
-  picker: {
-    height: 50,
-    color: '#53B6C7',
   },
   title: {
     fontSize: 22,
