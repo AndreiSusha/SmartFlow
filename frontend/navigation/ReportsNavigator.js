00b0295fdffb8d6d5fc19e6b582ad98e952c0fee
KostasV1l <r0926971@student.thomasmore.be>
2024-11-26 20:51:10 +0200

    changes to report details

--- frontend/navigation/ReportsNavigator.js
+++ frontend/navigation/ReportsNavigator.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReportsScreen from "../screens/Reports/ReportsScreen";
-import ReportDetails from "../screens/Reports/ReportDetails";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import ReportsScreen from '../screens/Reports/ReportsScreen';
+import ReportDetails from '../screens/Reports/ReportDetails';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 const ReportsNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{headerBackTitle: ""}}>
-      <Stack.Screen name="ReportsOverview" component={ReportsScreen}/>
+    <Stack.Navigator screenOptions={{ headerBackTitle: '' }}>
+      <Stack.Screen name="ReportsOverview" component={ReportsScreen} />
       <Stack.Screen name="Report" component={ReportDetails} />
     </Stack.Navigator>
   );
