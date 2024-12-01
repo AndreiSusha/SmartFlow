1eaec706b8bc859c70882e927a4b4ecbdfb25908
KostasV1l <r0926971@student.thomasmore.be>
2024-12-01 15:38:44 +0200

    added custom reusable text input component, made slight adjustments to the login screen

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,15 +1,7 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin", 
-      [
-        "module-resolver",
-        {
-          alias: {
-            '@styles': "./styles/*",
-          },
-        }
-      ]],
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin']
   };
 };
--- frontend/styles/Colors.js
+++ frontend/styles/Colors.js
@@ -2,5 +2,6 @@ export const Colors = {
     primary: "#53B6C7",
     secondary: "#A0C287",
     white: "#FFFFFF",
-    background: "#F2F2F2"
+    background: "#F2F2F2",
+    gray: "#4A4A4A"
 }
