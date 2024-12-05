323c2ef1cbc19fc2e8fc88a42b76f6e4ca1db8a9
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-05 19:56:27 +0200

    component created

---
+++ frontend/components/reports/charts/DonutChart.js
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import { StyleSheet, View, Animated, TextInput } from 'react-native';
+import Svg, { G, Circle } from 'react-native-svg';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedInput = Animated.createAnimatedComponent(TextInput);
+
+export default function DonutChart({
+  persentage = 0,
+  radius = 40,
+  strokeWidth = 10,
+  duration = 500,
+  color = 'tomato',
+  delay = 250,
+  textColor,
+  max = 100,
+  animate = false,
+}) {
+  const animatedValue = React.useRef(new Animated.Value(0)).current;
+  const circleRef = React.useRef();
+  const inputRef = React.useRef();
+  const halfCircle = radius + strokeWidth;
+  const circleCircumference = 2 * Math.PI * radius;
+
+  const runAnimation = () => {
+    animatedValue.setValue(0);
+    Animated.timing(animatedValue, {
+      toValue: persentage,
+      duration,
+      delay,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  React.useEffect(() => {
+    if (animate) {
+      runAnimation();
+    }
+  }, [animate]);
+
+  React.useEffect(() => {
+    animatedValue.addListener((v) => {
+      if (circleRef?.current) {
+        const maxPerc = (100 * v.value) / max;
+        const strokeDashoffset =
+          circleCircumference - (circleCircumference * maxPerc) / 100;
+        circleRef.current.setNativeProps({
+          strokeDashoffset,
+        });
+      }
+      if (inputRef?.current) {
+        inputRef.current.setNativeProps({
+          text: `${Math.round(v.value)}`,
+        });
+      }
+    });
+
+    return () => {
+      animatedValue.removeAllListeners();
+    };
+  }, [max, persentage]);
+
+  return (
+    <View style={{ width: radius * 2, height: radius * 2 }}>
+      <Svg
+        width={radius * 2}
+        height={radius * 2}
+        viewBox={`0 0 ${halfCircle * 2} ${halfCircle * 2}`}
+      >
+        <G rotation="-90" origin={`${halfCircle}, ${halfCircle}`}>
+          <Circle
+            cx="50%"
+            cy="50%"
+            stroke={color}
+            strokeWidth={strokeWidth}
+            r={radius}
+            fill="transparent"
+            strokeOpacity={0.2}
+          />
+          <AnimatedCircle
+            ref={circleRef}
+            cx="50%"
+            cy="50%"
+            stroke={color}
+            strokeWidth={strokeWidth}
+            r={radius}
+            fill="transparent"
+            strokeDasharray={circleCircumference}
+            strokeDashoffset={circleCircumference}
+            strokeLinecap="round"
+          />
+        </G>
+      </Svg>
+      <AnimatedInput
+        ref={inputRef}
+        underlineColorAndroid="transparent"
+        editable={false}
+        defaultValue="0"
+        style={[
+          StyleSheet.absoluteFillObject,
+          styles.text,
+          { fontSize: radius / 2, color: textColor ?? color },
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: '900',
+    textAlign: 'center',
+  },
+});
